74ab85767147bd65471e5140ca43692e21081d5c
Timofey <timofey20ab@gmail.com>
2021-03-26 11:14:54 +0700

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld v-for="(todo, index) in todos" :key="index" :todo="todo" />
+
+    <div v-on:click="showText">{{ active }}</div>
+    <input v-if="text" v-model="active" />
+    <div v-else>{{ textReverse }}</div>
+    <div v-bind:class="bind">
+      {{ text ? "bind" : "feter" }}
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      todos: [
+        { id: 1, name: "Tim", lastName: "Borisyuk" },
+        { id: 2, name: "123", lastName: "Borisyuk" },
+        { id: 3, name: "321", lastName: "Borisyuk" },
+      ],
+      active: "abcd",
+      bind: "bind",
+      text: true,
+    };
+  },
+  methods: {
+    showText() {
+      this.text = !this.text;
+    },
+  },
+  computed: {
+    textReverse() {
+      return this.active
+        .split("")
+        .reverse()
+        .join("");
+    },
+  },
+  watch: {
+    text() {
+      console.log("text изменился");
+    },
+  },
+};
 </script>
 
 <style>
@@ -24,5 +61,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  max-width: 1000px;
+  height: 600px;
+}
+img {
+  /* display: block; */
+  max-width: 100%;
+}
+.bind {
+  color: red;
+}
+.feter {
+  color: green;
 }
 </style>
